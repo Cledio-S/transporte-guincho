9459580481cf116f0a23cdcf8599e5e07be245b6
Eliézer M. <site.guincho0@gmail.com>
2025-11-04 15:16:49 -0300

    "Primeiro commit: site Transporte Guincho"

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/* ----------------------------------- *\
+  # NAVBAR TOGGLE
+\* ----------------------------------- */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+const overlay = document.querySelector("[data-overlay]");
+
+for (let i = 0; i < navTogglers.length; i++) {
+  navTogglers[i].addEventListener("click", () => {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+}
+
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", () => {
+    navbar.classList.remove("active");
+    overlay.classList.remove("active");
+  });
+}
+
+/* ----------------------------------- *\
+  # HEADER
+\* ----------------------------------- */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
